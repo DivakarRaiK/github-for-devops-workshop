dff7e606ab31e93aebf6b86b7e013ce9548df0fb
divakaraik <raidivakar26@gmail.com>
2026-01-29 17:46:55 +0000

    jenkinsfile added

---
+++ jenkins/jenkinsfile.sh
@@ -0,0 +1,37 @@
+pipeline{
+    agent any
+
+    stages{
+        stage('Hostname'){
+            steps{
+                sh 'hostname'
+            }
+        }
+
+        stage('Server Date'){
+            steps{
+                sh 'date'
+            }
+        }
+
+        stage('CPU Details'){
+            steps{
+                sh 'lscpu'
+            }
+        }
+
+        stage('Memory Usage'){
+            steps{
+                sh 'free -h'
+            }
+        }
+        stage('Disk usage'){
+            steps{
+                sh 'df -kh'
+            }
+        }
+
+
+    }
+    
+}
